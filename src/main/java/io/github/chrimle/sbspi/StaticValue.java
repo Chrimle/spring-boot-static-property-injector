b196b2d2ddbf8938f017b03416edcde72734ea8c
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-06-08 23:25:11 +0200

    Include Sources and JavaDocs in Maven Central Publishing

--- src/main/java/io/github/chrimle/sbspi/StaticValue.java
+++ src/main/java/io/github/chrimle/sbspi/StaticValue.java
@@ -5,8 +5,45 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Similar to Spring's {@code Value}-annotation, this annotation can be applied to {@code static}
+ * fields. This annotation supports <a
+ * href="https://docs.spring.io/spring-framework/docs/3.0.x/reference/expressions.html">SpEL (Spring
+ * Expression Language)</a> and/or <a
+ * href="https://docs.spring.io/spring-framework/reference/core/beans/annotation-config/value-annotations.html">Spring
+ * property placeholders</a>.
+ *
+ * <p>Example
+ *
+ * <pre>{@code
+ * public class Example {
+ *
+ *   // Using Spring property placeholder
+ *   @StaticValue("${my.custom.property}")
+ *   private static String exampleWithoutDefault;
+ *
+ *   // Spring property placeholder with default value, defaults to "someDefault"
+ *   @StaticValue("${my.custom.other-property:someDefault}")
+ *   private static String exampleWithSomeDefault;
+ * }
+ * }</pre>
+ *
+ * <p>Note that actual processing of the {@code StaticValue} annotation is performed by {@link
+ * StaticValueInjector}.
+ *
+ * @see org.springframework.beans.factory.annotation.Value
+ * @author Chrimle
+ * @since 0.1.0
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface StaticValue {
+  /**
+   * The value expression, such as {@code #{systemProperties.myProp}}, or property placeholder such
+   * as {@code ${my.app.myProp}}.
+   *
+   * @return the value of this annotation.
+   * @since 0.1.0
+   */
   String value();
 }
--- src/main/java/io/github/chrimle/sbspi/StaticValueInjectorException.java
+++ src/main/java/io/github/chrimle/sbspi/StaticValueInjectorException.java
@@ -1,11 +1,32 @@
 package io.github.chrimle.sbspi;
 
+/**
+ * Exception thrown by {@link StaticValueInjector} when processing {@link StaticValue}-annotations.
+ *
+ * @since 0.1.0
+ * @author Chrimle
+ */
 public class StaticValueInjectorException extends RuntimeException {
 
+  /**
+   * Exception thrown by {@link StaticValueInjector} when processing {@link
+   * StaticValue}-annotations.
+   *
+   * @param message describing the issue in more detail.
+   * @since 0.1.0
+   */
   public StaticValueInjectorException(final String message) {
     super(message);
   }
 
+  /**
+   * Exception thrown by {@link StaticValueInjector} when processing {@link
+   * StaticValue}-annotations.
+   *
+   * @param message describing the issue in more detail.
+   * @param cause of this exception.
+   * @since 0.1.0
+   */
   public StaticValueInjectorException(final String message, final Throwable cause) {
     super(message, cause);
   }
