353a342a7ea45fb3e1a29aec2b121a364a4f3a9a
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-06-08 19:09:56 +0200

    Add `StaticValue.java`

---
+++ src/main/java/io/github/chrimle/sbspi/StaticValue.java
@@ -0,0 +1,12 @@
+package io.github.chrimle.sbspi;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface StaticValue {
+  String value();
+}
